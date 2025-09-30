6077718a710063b74a1361776da734811513297e
QuocAn108 <anhoang800140@gmail.com>
2025-09-30 23:58:33 +0700

    a description of your change here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stats import get_num_words, get_char_counts
-BOOK_PATH = "books/frankenstein.txt"
+import sys
+from stats import get_num_words, get_char_counts, sort_char_counts
 
 
 def get_book_text(path):
@@ -8,18 +8,33 @@ def get_book_text(path):
             file_contents = f.read()
             return file_contents
     except FileNotFoundError:
-        return f"Lỗi: Không tìm thấy file tại đường dẫn {path}"
+        return f"Error: File not found at {path}"
     except Exception as e:
-        return f"Đã xảy ra lỗi khi đọc file: {e}"
+        return f"An error occurred while reading the file: {e}"
 
 
 def main():
-    book_text = get_book_text(BOOK_PATH)
-    print(book_text)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    if "Error:" in book_text:
+        print(book_text)
+        sys.exit(1) 
     num_words = get_num_words(book_text)
+    char_counts_dict = get_char_counts(book_text)
+    sorted_char_list = sort_char_counts(char_counts_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    char_counts = get_char_counts(book_text)
-    print(char_counts)
+    print("--------- Character Count -------")
+    for item in sorted_char_list:
+        char = item["char"]
+        count = item["num"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -9,3 +9,16 @@ def get_char_counts(text):
         lowered_char = char.lower()
         counts[lowered_char] = counts.get(lowered_char, 0) + 1
     return counts
+
+
+def sort_on(item):
+    return item["num"]
+
+
+def sort_char_counts(char_counts_dict):
+    sorted_list = []
+    for char, count in char_counts_dict.items():
+        if char.isalpha():
+            sorted_list.append({"char": char, "num": count})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
